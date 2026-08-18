f247389ba684de706cdbcbfb176c09c4ae152702
aetos <aetos382@users.noreply.github.com>
2026-08-18 21:52:31 +0900

    comment

--- sandbox/SourceGenerator/FileLocal.cs
+++ sandbox/SourceGenerator/FileLocal.cs
@@ -2,7 +2,8 @@
 
 namespace Case4;
 
-// file ローカル型を外側型としてネストすると、生成コードが別の型になってしまう（Source Generator では file partial に対してコードを足せない）。
+// file ローカル型を外側型としてネストすると、生成コードが別の型になってしまう（生成ファイルに出力される Case4.FileHolder は関係ない別の型）
+// Source Generator では file partial に対してコードを足せない。
 // このケースは無視するか警告を出すべき。
 file partial class FileHolder
 {
