4541544f5347b65b4ff39575ba95e048f33db570
aetos <aetos382@users.noreply.github.com>
2026-08-18 19:42:07 +0900

    sandbox

---
+++ sandbox/SourceGenerator/Collection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using MemoryPack;
+
+namespace Case1;
+
+// MemoryPackFormatter.g.cs に Holder が出力されない（Case1.NestedList になってしまう）
+public partial class Holder
+{
+    [MemoryPackable(GenerateType.Collection)]
+    public partial class NestedList : List<int>
+    {
+    }
+}
+
+[MemoryPackable(GenerateType.Collection)]
+public partial class MyList : List<int>
+{
+}
---
+++ sandbox/SourceGenerator/Formatter.cs
@@ -0,0 +1,22 @@
+﻿using MemoryPack;
+
+namespace Case2;
+
+public interface IExternalUnion;
+
+[MemoryPackable]
+public partial class ExternalImpl : IExternalUnion
+{
+    public int X { get; set; }
+}
+
+// MemoryPackFormatter.g.cs に Holder が出力されない（Case2.ExternalUnionFormatter になってしまう）。
+// そのため、この Formatter は使われない。
+public partial class Holder
+{
+    [MemoryPackUnionFormatter(typeof(IExternalUnion))]
+    [MemoryPackUnion(0, typeof(ExternalImpl))]
+    public partial class ExternalUnionFormatter
+    {
+    }
+}
