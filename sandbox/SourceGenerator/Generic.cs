54bbcf093823dbd198997530d590dc2a3e3e6dab
aetos <aetos382@users.noreply.github.com>
2026-08-18 21:54:30 +0900

    comment

--- sandbox/SourceGenerator/Generic.cs
+++ sandbox/SourceGenerator/Generic.cs
@@ -3,8 +3,6 @@
 namespace Case3;
 
 // MemoryPackFormatter.g.cs に外側型の型引数が出力されない（partial class GenericOuter になってしまう）。
-// アリティ 0 の別の型なので、生成コードは Inner とは別のデコイ型を宣言することになり、
-// CS1061 / CS0117（'GenericOuter.Inner' に 'X' の定義がない）になる。
 // GetContainingTypeDeclarations() が containingType.Name を使っており、型引数を落としているため。
 public partial class GenericOuter<T>
 {
@@ -15,9 +13,7 @@ public partial class GenericOuter<T>
     }
 }
 
-// union パスでも同じ。メンバー型を外側型の外に出せば、属性引数の CS0416 を回避して到達できる。
-// CS0246（型 'T' が見つからない）/ CS7003（バインドされていないジェネリック名）のほか、
-// GenericUnionOuter.IUnion と GenericUnionOuter<T>.IUnion が並ぶ CS1503 になる。
+// union パスでも同じ。
 public partial class GenericUnionOuter<T>
 {
     [MemoryPackable]
