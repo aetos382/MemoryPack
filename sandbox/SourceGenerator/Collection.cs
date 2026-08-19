e8f9a6fd5d62e9e6dc5429951b41806ff6ad3bb5
aetos <aetos382@users.noreply.github.com>
2026-08-19 16:22:52 +0900

    comment

--- sandbox/SourceGenerator/Collection.cs
+++ sandbox/SourceGenerator/Collection.cs
@@ -5,6 +5,7 @@ using MemoryPack;
 namespace Case1;
 
 // MemoryPackFormatter.g.cs に Holder が出力されない（Case1.NestedList になってしまう）
+// PR #386 で直る
 public partial class Holder
 {
     [MemoryPackable(GenerateType.Collection)]
